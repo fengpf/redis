7b066d30ccbb783fb489cbdfcc5a1406f009f010
fengpf <18221167541@163.com>
2020-03-16 15:29:33 +0800

    init

---
+++ deps/hiredis/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /data/app/redis/deps/hiredis
+# Build directory: /data/app/redis/deps/hiredis
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(hiredis-test "/data/app/redis/deps/hiredis/test.sh")
+set_tests_properties(hiredis-test PROPERTIES  _BACKTRACE_TRIPLES "/data/app/redis/deps/hiredis/CMakeLists.txt;83;ADD_TEST;/data/app/redis/deps/hiredis/CMakeLists.txt;0;")
